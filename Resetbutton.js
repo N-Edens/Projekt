7134d8e83107a4cc11caaf81da55efdb1b970904
name <Nicolaiedens@gmail.com>
2023-05-15 13:49:58 +0200

    Add existing file

---
+++ Resetbutton.js
@@ -0,0 +1,24 @@
+var resetBtn = document.getElementById("resetBtn");
+
+function resetAnimation() {
+  var cars = document.querySelectorAll(".car");
+
+  cars.forEach(function(car) {
+    car.style.animation = "";
+    car.style.animationPlayState = "paused";
+    car.style.transform = "";
+    car.style.left = "";
+  });
+
+  // Remove active class from both start and stop buttons
+  startBtn.classList.remove("active");
+  stopBtn.classList.remove("active");
+
+  startBtn.disabled = false;
+}
+
+resetBtn.addEventListener("click", function() {
+  clearInterval(animationInterval);
+  resetAnimation();
+  resetBtn.classList.add("active");
+});
